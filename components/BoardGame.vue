e3dffa07d81a4b668cc93d7edd2cbb7c6d1b4ffb
Kathleen Santos <kathleen.crissantos15@gmail.com>
2024-04-24 17:24:53 -0300

    [Struct] restructuring component base

--- components/BoardGame.vue
+++ components/BoardGame.vue
@@ -13,14 +13,18 @@
 <script lang="ts">
 // import defineComponent from 'nuxt'
 
-
 export default{
   data() {
     return {
       bgColorRefBoard: ref('green'),
       sizeSquares: 24,
       qtdSquares: 30,
+      snakeTrail: [],
     }
+  },
+  
+  mounted() {
+    this.createBoard()
   },
 
   computed: {
@@ -30,33 +34,33 @@ export default{
     console.log('Component has been created')
   },
 
-  mounted() {
-    const refBoard = this.$refs.board as HTMLCanvasElement;
-    const ctx = refBoard?.getContext('2d');
-
-    if (ctx) {
-      const size = this.sizeSquares;
-      const qtdSquares = this.qtdSquares;
-
-      for (let row = 0; row < qtdSquares; row++) {
-        for (let col = 0; col < qtdSquares; col++) {
-          const x = col * size;
-          const y = row * size;
-
-          if ((row + col) % 2 === 0) {
-            ctx.fillStyle = '#92DC70'; // Light
-          } else {
-            ctx.fillStyle = '#85CB64'; // Dark
-          }
-
-          ctx.fillRect(x, y, size, size);
-        }
-      }
-    }
-  },
 
   // Methods
   methods: {
+    createBoard () {
+      const refBoard = this.$refs.board as HTMLCanvasElement;
+      const ctx = refBoard?.getContext('2d');
+
+      if (ctx) {
+        const size = this.sizeSquares;
+        const qtdSquares = this.qtdSquares;
+
+        for (let row = 0; row < qtdSquares; row++) {
+          for (let col = 0; col < qtdSquares; col++) {
+            const x = col * size;
+            const y = row * size;
+
+            if ((row + col) % 2 === 0) {
+              ctx.fillStyle = '#92DC70'; // Light
+            } else {
+              ctx.fillStyle = '#85CB64'; // Dark
+            }
+
+            ctx.fillRect(x, y, size, size);
+          }
+        }
+      }
+    }
   },
 };
 </script>
